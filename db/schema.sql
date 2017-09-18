7b2d610b35b3b782dc1656fa67a0329515d695e0
Gildon Kirk <gildonkirk@gmail.com>
2017-09-18 12:34:55 -0400

    initial commit

---
+++ db/schema.sql
@@ -0,0 +1,8 @@
+USE burger_db;
+CREATE TABLE burgers (
+	id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR (255) NOT NULL,
+    devoured BOOLEAN DEFAULT false,
+    date TIMESTAMP,
+    PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Double Double');
+INSERT INTO burgers (burger_name) VALUES ('Big Mac');
+INSERT INTO burgers (burger_name) VALUES ('Quarter Pounder');
+INSERT INTO burgers (burger_name) VALUES ('Del Burger');
+INSERT INTO burgers (burger_name) VALUES ('Whopper');
+INSERT INTO burgers (burger_name) VALUES ('Double Whopper');
